85ca9a545e716d9074d68d5cfc6dca7dcdac9784
Smaug73 <52470494+Smaug73@users.noreply.github.com>
2019-11-26 16:32:38 +0100

    aggiornamento

--- tomato_ai_ver2_keras.py
+++ tomato_ai_ver2_keras.py
@@ -24,7 +24,7 @@ data_dir='C:\\Users\\stefr\\Desktop\\TESI DATASET\\PlantVillage'
 IMG_HEIGHT=256
 IMG_WIDTH=256
 
-epochs=15
+epochs=20
 
 
 image_generator = ImageDataGenerator(rescale=1./255,validation_split=0.3)
@@ -78,22 +78,24 @@ model = Sequential([
 
 #Costruiamo una architettura che possieda i livelli di dropout, in modo da evitare l'overfitting
 #Aumenteremo inoltre il numero di epoche.Questa tipologia sarà l'architettura definitiva della tesi(per ora).
+#AGGIUNGERE batch normalization, riscala l'ordine di grandezza dell'ordine dei pesi interni, da inserire o come primo livello o prima dei dense
 model = Sequential([
     keras.layers.Conv2D(8, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
     keras.layers.MaxPooling2D(),   
-    keras.layers.Dropout(0.2),      #Un livello di dropout dopo il primo maxPooling 
+    keras.layers.Dropout(0.2),                      #Un livello di dropout dopo il primo maxPooling 
     keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
     keras.layers. MaxPooling2D(),
-    keras.layers.Dropout(0.2),      #Un livello di dropout dopo l'ultimo livello di maxPooling 
-    keras.layers.Flatten( ),      #Questo primo layer serve solo a riformattare i pixel, disponendoli su di una unica dimensione
+    keras.layers.Dropout(0.2),                      #Un livello di dropout dopo l'ultimo livello di maxPooling 
+    keras.layers.Flatten( ),                        #Questo primo layer serve solo a riformattare i pixel, disponendoli su di una unica dimensione
+    keras.layers.BatchNormalization(),              #Aggiunto il batchNormalization
     keras.layers.Dense(64, activation='relu'),      #Questo secondo livello e collegato al primo ed e' formato da 128 neuroni
     keras.layers.Dense(10, activation='softmax')
 ])
 
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam',                         #provare ad utilizzare sgd con learning rate elevato e provarlo con diversi paramentri
+              loss='sparse_categorical_crossentropy',   #usare la libreria hyperopt per modificare i parametri e fare più prove, iniziare con un solo parametro per evita di perdere il controllo
+              metrics=['accuracy'])                     
 
 model.summary()
 
@@ -108,7 +110,7 @@ history = model.fit_generator(
 
 # Save the model
 #history.save('C:\\Users\\stefr\\Desktop\\TESI DATASET\\Modelli\\model_v2.h5')
-model.save('C:\\Users\\stefr\\Desktop\\TESI DATASET\\Modelli\\model_v_definitiva_1.h5')
+model.save('C:\\Users\\stefr\\Desktop\\TESI DATASET\\Modelli\\model_v_definitiva_3.h5')
 
 #print(history.history[])
 
